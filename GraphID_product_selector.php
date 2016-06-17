86b607d4db8411b2804538b9fba8d4a8d7d40e96
GraphID <n.hekkink@graphid.nl>
2016-06-17 12:59:24 +0200

    Add new taxonomies easy
    Just copy and paste the function incl. the action and rename it. Then type the new taxonomy (singular and plural) and we're done!

--- GraphID_product_selector.php
+++ GraphID_product_selector.php
@@ -7,12 +7,10 @@ Author: GraphID, Norbert Hekkink
 Author URI: http://www.graphid.nl/
 Version: 0.0.1
 */
-
 //Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
  
 // Toevoegen custom post type
  
@@ -68,15 +66,14 @@ function grwp_register_post_type() {
 			'custom-fields'
 		)
 	);
-	register_post_type( 'Emax products', $args);
+	register_post_type( 'emax', $args);
 }
 add_action( 'init', 'grwp_register_post_type' );
 
 function grwp_register_taxonomy() {
-	
-$singular = 'Location';
-$plural = 'Locations';
-
+$singular = 'Printing method';
+$plural = 'Printing methods';
+$slug = str_replace( ' ', '_', strtolower( $singular ) );
 $labels = array(
 	'name'				=> $plural,
 	'singular_name'			=> $singular,
@@ -95,7 +92,6 @@ $labels = array(
 	'not_found'			=> 'No ' . $plural . ' found.',
 	'menu_name'			=> $plural,
 );
-
 $args = array(
 	'hierarchical'		=> true,
 	'labels'		=> $labels,
@@ -103,8 +99,45 @@ $args = array(
 	'show_admin_column'	=> true,
 	'update_count_callback'	=> '_update_post_term_count',
 	'query_var'		=> true,
-	'rewrite'		=> array( 'slug' => 'location' ),
+	'rewrite'		=> array( 'slug' => $slug ),
 );
-register_taxonomy( 'location', 'job', $args );
+register_taxonomy( $slug, 'emax', $args );
 }
 add_action( 'init', 'grwp_register_taxonomy' );
+
+// Nieuwe taxonomy
+
+function grwp_register_taxonomy2() {
+$singular = 'Material';
+$plural = 'Materials';
+$slug = str_replace( ' ', '_', strtolower( $singular ) );
+$labels = array(
+	'name'				=> $plural,
+	'singular_name'			=> $singular,
+	'search_items'			=> 'Search ' . $plural,
+	'popular_items'			=> 'Popular ' . $plural,
+	'all_items'			=> 'All ' . $plural,
+	'parent_item'			=> null,
+	'parent_item_colon'		=> null,
+	'edit_item'			=> 'Edit ' . $singular,
+	'update_item'			=> 'Update ' . $singular,
+	'add_new_item'			=> 'Add New ' . $singular,
+	'new_item_name'			=> 'New ' . $singular . ' Name',
+	'separate_items_with_commas' 	=> 'Separate ' . $plural . ' with commas',
+	'add_or_remove_items'		=> 'Add or remove ' . $plural,
+	'choose_from_most_used'		=> 'Choose from the most used ' . $plural,
+	'not_found'			=> 'No ' . $plural . ' found.',
+	'menu_name'			=> $plural,
+);
+$args = array(
+	'hierarchical'		=> true,
+	'labels'		=> $labels,
+	'show_ui'		=> true,
+	'show_admin_column'	=> true,
+	'update_count_callback'	=> '_update_post_term_count',
+	'query_var'		=> true,
+	'rewrite'		=> array( 'slug' => $slug ),
+);
+register_taxonomy( $slug, 'emax', $args );
+}
+add_action( 'init', 'grwp_register_taxonomy2' );
